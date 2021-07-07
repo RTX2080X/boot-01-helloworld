04aad958b61adc43987df213ded110d01d9314b4
haozequn <haozequn920@iCloud.com>
2021-07-07 21:49:15 +0800

    -@Configurations

--- src/main/java/com/boot/controller/HelloworldController.java
+++ src/main/java/com/boot/controller/HelloworldController.java
@@ -1,12 +1,12 @@
-package com.boot.conntroller;
+package com.boot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HelloworldController {
     @RequestMapping("/hello")
-    public String handle01() {
-        return "Hello Spring";
+    public String helloworld() {
+        return "Hello World";
     }
 }
